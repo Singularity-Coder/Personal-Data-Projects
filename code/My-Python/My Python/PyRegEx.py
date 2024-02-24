b1107be39a8e365d26baa2599cbb1f3fef1f61e3
Singularity Coder <codehithesh@gmail.com>
2024-02-24 14:08:55 +0530

    regex

--- code/My-Python/My Python/PyRegEx.py
+++ code/My-Python/My Python/PyRegEx.py
@@ -6,19 +6,34 @@ import re
 """
 RegEx funcs
 
-1. findall - returns a list containing all matches
+1. findall - returns a list containing all matches else empty list
 2. search - returns a match obj if there is a match anywhere in the str
 3. split - returns a list where the str has been split at each match
 4. sub - replaces 1 or many matches with a str
 """
 
+text = "The universe is ~ 13.7 billion years old."
+
+print(re.search("rse is", text)) # returns Match obj
+print(re.search("rse is", text).span()) # tuple of start n end pos
+print(re.search("rse is", text).string)
+print(re.search("rse is", text).group()) # pos of match start
+print(re.search("rse is", text).start()) # gets start position of the search str
+print(re.search("kakarot", text)) # if no match found gives None
+print()
+print(re.split(r"\s", text))
+print(re.split(r"\s", text, 1))
+print(re.split(r"\s", text, 3)) # splits into 3 parts
+print()
+print(re.sub(r"\s", "   ", text))
+print(re.sub(r"\s", "   ", text, 4)) # set max replacements
+
+
 print("\nMETA CHARACTERS -------------------------------------------------------------------------------------\n")
 # chars with special meaning
 
-text = "The universe is ~ 13.7 billion years old."
-
 print(re.findall("[a-m]", text))
-print(re.findall("\\d", text))
+print(re.findall(r"\d", text))
 print()
 print(re.findall("^The", text)[0] == "The")
 print(re.findall("old.$", text)[0] == "old.")
@@ -35,8 +50,7 @@ print()
 # followed by 1 or more chars, and a char
 print(re.findall("un.+se", text))
 print(re.findall("un.+e", text))
-print(re.findall("d.+", text))
-print(re.findall("d\..+", text))
+print(re.findall("d.+", text)) # how to include period? escape not working
 print(re.findall("dance.+", text))
 print()
 # followed by 0 or 1 (any) character, and a char
@@ -58,11 +72,11 @@ print(re.findall("The|old|universe", text))
 print(re.findall("(d.+)(The|old)", text)) # not correct
 
 print("\nSPECIAL SEQUENCES -------------------------------------------------------------------------------------\n")
-# A special seq is a \ followed by a char. In pycharm you must escape that \ with a \
+# A special seq is a \ followed by a char. In pycharm you must escape that \ with a \ or use a raw string r""
 
 # check if string starts with "The"
-print(re.findall("\\AThe", text))
-print(re.findall("\\Ais", text))
+print(re.findall(r"\AThe", text))
+print(re.findall(r"\Ais", text))
 print()
 # check if "uni" is present at the beginning of a WORD:
 print(re.findall(r"\buni", text)) # r"" at the start is to enforce raw string
@@ -74,8 +88,33 @@ print(re.findall(r"ol\b", text))
 print(re.findall(r"\Bniv", text))
 print(re.findall(r"\Ber", text))
 print(re.findall(r"\Bks", text))
-# Check if "niv" is present, but NOT at the end of a word
-print(re.findall(r"niv\B", text))
+# check if "niv" is present, but NOT at the end of a word. Anything in between start n end chars
+print(re.findall(r"rs\B", text))
+print(re.findall(r"rse\B", text))
+print()
+print(re.findall(r"\d", text)) # check if the string contains any digits (numbers from 0-9):
+print(re.findall(r"\D", text)) # no-digit char
+print()
+print(re.findall(r"\s", text)) # white-space char
+print(re.findall(r"\S", text)) # NON white-space char
+print()
+print(re.findall(r"\w", text)) # chars from a to Z, digits from 0-9, and the underscore _ char
+print(re.findall(r"\W", text)) # chars NOT between a and Z. Like "!", "?" white-space etc.
+print()
+print(re.findall(r"old\.\Z", text)) # has "old." at the end of str
+
+print("\nSETS -------------------------------------------------------------------------------------\n")
+
+# set of chars in []
+print(re.findall("[aei]", text)) # has any of a e i chars
+print(re.findall("[a-f]", text))
+print(re.findall("[^efghijklmnopqrstuv]", text))
+print(re.findall("[0123]", text))
+print(re.findall("[0-9]", text))
+print(re.findall("[0-5][0-9]", text)) # two-digit numbers, from 00 to 59
+print(re.findall("[a-mN-T]", text))
+print(re.findall("[~]", text)) # In sets, +, *, ., |, (), $,{} has no special meaning
+
 
 
 
